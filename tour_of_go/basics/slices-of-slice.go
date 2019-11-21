ed9a116ce920221c918525f1afce67fced7e50fe
kiakiraki <jas39.gripen@gmail.com>
2019-11-21 17:08:54 +0900

    WIP: moretypes

---
+++ tour_of_go/basics/slices-of-slice.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	board := [][]string{
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+	}
+
+	board[0][0] = "X"
+	board[2][2] = "O"
+	board[1][2] = "X"
+	board[1][0] = "O"
+	board[0][2] = "X"
+
+	for i := 0; 1 < len(board); i++ {
+		fmt.Printf("%s\n", string.Join(board[i], " "))
+	}
+}
